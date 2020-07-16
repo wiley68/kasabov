cb40ca795c5fadecba72ea17399f874a1cdc5af8
wiley68 <ilko.iv@gmail.com>
2020-07-16 16:11:25 +0300

    front layout

--- resources/views/layouts/frontLayout/front_design.blade.php
+++ resources/views/layouts/frontLayout/front_design.blade.php
@@ -4,7 +4,6 @@
 <html lang="en">
 <head>
 <title>PartyBox</title>
-@endif
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="csrf-token" content="{{ csrf_token() }}" />
